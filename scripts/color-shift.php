328dd2c1e8f519a8068265aaba4909fa235232ec
Jen Wachter <jwachter@jhu.edu>
2024-06-07 10:30:13 -0400

    Updated color shift script to have two mode: table and json
    When table, prints out a table demonstrating the color shifts. When json, creates and formats config/colors with the shifted colors.

--- scripts/color-shift.php
+++ scripts/color-shift.php
@@ -1,117 +1,127 @@
 <?php
 
 /**
- * Shifts brand colors outside of a grade range to within a grade range
+ * Shifts brand colors outside of a grade range to within a grade range.
+ * In addition to adjusting the RGB and hex values, the following data is added:
+ * - Grade
+ * - Luminance
  */
 
+use JohnsHopkins\Color\Calculate;
+use JohnsHopkins\Color\Convert;
 use JohnsHopkins\Color\Grades;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$json = file_get_contents(dirname(__DIR__) . '/config/colors.json');
+// // show shifted colors in a table
+// $mode = 'table';
+
+// save shifted colors to config/colors.json
+$mode = 'json';
+
+$saveJSON = false;
+$showTable = true;
+
+$json = file_get_contents(dirname(__DIR__) . '/config/brand-colors.json');
 $colors = json_decode($json, true);
 
 $grades = new Grades(false);
 
-// colors that fall outside of a range
-$colors = [
-  [$colors[3]],
-  [$colors[4], null, true],
-  [$colors[6]],
-  [$colors[7], null, true],
-  [$colors[8], null, true],
-  [$colors[13], 80], // maroon
-  [$colors[15], null, true],
-  [$colors[16], null, true],
-  [$colors[17], null, true],
-  [$colors[20], null, true],
+// colors that need a shift
+$shift = [
+  4 => [],
+  5 => [null, true],
+  8 => [null, true],
+  9 => [null, true],
+  14 => [80], // maroon (so we can have two reds in the palette)
+  16 => [null, true],
+  17 => [null, true],
+  18 => [null, true],
+  21 => [null, true],
 ];
 
-echo "<table style='table-layout: fixed;' cellpadding='10' cellspacing='0' border='1'>";
-echo "<thead><tr><th>Name</th><th>Old</th><th>New</th></tr></thead>";
+function contrastWithSable(array $color): float
+{
+  $sable = [49, 38, 29];
 
-echo "<tbody>";
-$rows = array_map(function ($c) use ($grades) {
+  $sableLum = Calculate::luminance($sable);
+  $colorLum = Calculate::luminance($color['rgb']); // do not use rounded version in $color['luminance']
 
-  $color = $c[0];
-  $grade = $c[1] ?? null;
-  $opposite = $c[2] ?? false;
+  if ($sableLum > $colorLum) {
+    // double black
+    return Calculate::contrast($sable, $color['rgb']);
+  }
 
-  echo "<tr>";
+  return Calculate::contrast($color['rgb'], $sable);
+}
 
-  echo "<td>{$color['name']}</td>";
+$colors = array_map(static function ($color) use ($grades, $mode, $shift) {
 
-  $oldGrade = $grades->findGradeOfRGB($color['rgb']);
+  $id = $color['id'];
 
-  $rgb = 'rgb(' . implode(",", $color['rgb']) . ')';
-  echo "<td style='background-color:$rgb; color: #fff;'>$oldGrade</td>";
+  $grade = $grades->findGradeOfRGB($color['rgb']);
 
-  $new = $grades->shiftRGBtoGrade($color['rgb'], $grade, $opposite);
+  if (isset($shift[$id])) {
 
-  $rgb = 'rgb(' . implode(",", $new['color']) . ')';
-  echo "<td style='background-color:$rgb; color: #fff;'>{$new['grade']}</td>";
+    if ($mode === 'table') {
+      $color['shifted'] = true;
+      $color['original'] = [
+        'rgb' => $color['rgb'],
+        'hex' => $color['hex'],
+        'grade' => $grade,
+      ];
+    }
 
-  echo "</tr>";
+    $new = $grades->shiftRGBtoGrade($color['rgb'], $shift[$id][0] ?? null, $shift[$id][1] ?? false);
+
+    $color['rgb'] = $new['rgb'];
+    $color['hex'] = Convert::rgb_hex($new['rgb']);
+    $color['grade'] = $new['grade'];
+  } else {
+    $color['grade'] = $grade;
+  }
+
+  // standardize hex to lowercase
+  $color['hex'] = strtolower($color['hex']);
+
+  // add luminosity and contrast values
+  $color['luminance'] = round(Calculate::luminance($color['rgb']) * 100, 2);
+  $color['contrast'] = [
+    'double-black' => Calculate::contrast($color['rgb'], [0, 0, 0]),
+    'sable' => contrastWithSable($color),
+    'white' => Calculate::contrast([255, 255, 255], $color['rgb']),
+  ];
+
+  return $color;
 
 }, $colors);
-echo "</tbody></table>";
 
+if ($mode === 'json') {
 
-// // closest range
-// echo "<table style='table-layout: fixed;' cellpadding='10' cellspacing='0' border='1'>";
-// echo "<thead><tr><th>Name</th><th>Old</th><th>New</th></tr></thead>";
-//
-// echo "<tbody>";
-// $rows = array_map(function ($c) use ($grades) {
-//
-//   $color = $c[0];
-//
-//   echo "<tr>";
-//
-//   echo "<td>{$color['name']}</td>";
-//
-//   $oldGrade = $grades->findGradeOfRGB($color['rgb']);
-//
-//   $rgb = 'rgb(' . implode(",", $color['rgb']) . ')';
-//   echo "<td style='background-color:$rgb; color: #fff;'>$oldGrade</td>";
-//
-//   $new = $grades->shiftRGBtoGrade($color['rgb']);
-//
-//   $newRGB = $new['direction'] === 'up' ? $new['colors']['max'] : $new['colors']['min'];
-//   $rgb = 'rgb(' . implode(",", $newRGB) . ')';
-//   echo "<td style='background-color:$rgb; color: #fff;'>{$new['grade']}</td>";
-//
-//   echo "</tr>";
-//
-// }, $colors);
-// echo "</tbody></table>";
-//
-//
-// // farthest range
-// echo "<table style='table-layout: fixed;' cellpadding='10' cellspacing='0' border='1'>";
-// echo "<thead><tr><th>Name</th><th>Old</th><th>New</th></tr></thead>";
-//
-// echo "<tbody>";
-// $rows = array_map(function ($c) use ($grades) {
-//
-//   $color = $c[0];
-//
-//   echo "<tr>";
-//
-//   echo "<td>{$color['name']}</td>";
-//
-//   $oldGrade = $grades->findGradeOfRGB($color['rgb']);
-//
-//   $rgb = 'rgb(' . implode(",", $color['rgb']) . ')';
-//   echo "<td style='background-color:$rgb; color: #fff;'>$oldGrade</td>";
-//
-//   $new = $grades->shiftRGBtoGrade($color['rgb'], true);
-//
-//   $newRGB = $new['direction'] === 'up' ? $new['colors']['max'] : $new['colors']['min'];
-//   $rgb = 'rgb(' . implode(",", $newRGB) . ')';
-//   echo "<td style='background-color:$rgb; color: #fff;'>{$new['grade']}</td>";
-//
-//   echo "</tr>";
-//
-// }, $colors);
-// echo "</tbody></table>";
+  $json = json_encode($colors);
+  file_put_contents(dirname(__DIR__) . '/config/colors.json', $json);
+  exec('npm run format-json');
+
+} else if ($mode === 'table') {
+
+  echo "<table style='table-layout: fixed;' cellpadding='10' cellspacing='0' border='1'>";
+  echo "<thead><tr><th>Name</th><th>Old</th><th>New</th></tr></thead><tbody>";
+
+  array_filter(array_map(static function ($color) {
+
+    if (!isset($color['shifted'])) {
+      return null;
+    }
+
+    echo "<tr><td>{$color['name']}</td>";
+
+    $rgb = 'rgb(' . implode(",", $color['original']['rgb']) . ')';
+    echo "<td style='background-color:$rgb; color: #fff;'>{$color['original']['grade']}</td>";
+
+    $rgb = 'rgb(' . implode(",", $color['rgb']) . ')';
+    echo "<td style='background-color:$rgb; color: #fff;'>{$color['grade']}</td></tr>";
+
+  }, $colors));
+
+  echo "</tbody></table>";
+}
