19c70dc1cf5d6edf18ef0ef39380beaa833dd17e
Jen Wachter <jwachter@jhu.edu>
2024-06-07 12:27:08 -0400

    Included palettes in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 import * as calculate from './src/calculate.js';
 import * as colors from './src/colors.js';
 
-export { calculate, colors };
+import palettes from '../../config/palettes.json';
+
+export { calculate, colors, palettes };
